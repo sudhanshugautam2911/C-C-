2fe54b79deea87eeace6f889b955a1bbdb467cfe
sudhanshugautam2911 <gautamsudhanshu2911@gmail.com>
2022-08-19 17:15:04 +0530

    printing pair and subarray

---
+++ DSA UDEMY/1 Array/10_printing_subArray.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+void printing_subArray(int arr[], int n)
+{
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i; j < n; j++)
+        {
+            for (int k = i; k <= j; k++)
+            {
+                cout << arr[k] << ",";
+            }
+            cout << endl;
+        }
+    }
+}
+
+int main()
+{
+    int arr[] = {10, 20, 30, 40, 50, 60};
+    int n = sizeof(arr) / sizeof(int);
+
+    printing_subArray(arr, n);
+
+    return 0;
+}
